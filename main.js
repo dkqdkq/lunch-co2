1d5e452305f0d294219e8ed8b566fbd3e67c0221
dkq <dev.dkq@gmail.com>
2023-10-08 18:01:00 +0900

    우리 만나서 개뻘짓했음

--- main.js
+++ main.js
@@ -11,17 +11,19 @@ app.get('/', async (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+
 app.get('/api', async (req, res) => {
     const query = req.query;
-    const YYYYMMDD = query.date;
-    const data = await axios.get(`https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010209&Type=json&KEY=9e74b25e6e074c3bbeb16eaf1e20be1b&MLSV_YMD=${YYYYMMDD}`);
-    const meal = data.data?.mealServiceDietInfo?.row[0]?.DDISH_NM;
+    const YYYYMMDD = 20231010;
+    const data = await axios.get(`https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010144&Type=json&MLSV_YMD=${YYYYMMDD}`);
+    const meal = data.data?.mealServiceDietInfo?.[1]?.row?.[0].DDISH_NM;
     if (!meal) {
         res.status(404).json({ message: 'Not Found' });
     }
+    res.status(200).json({meal: meal});
 });
 
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+app.listen(30000, () => {
+    console.log('Server is listening on port 6974');
 });
