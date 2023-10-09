96ffbe4970a5a9f67a477883b212eea96ff2d7f8
JIN WOOHYUN <helgisnw@gmail.com>
2023-10-09 23:02:59 +0900

    schoolcode edit

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ app.get('/', async (req, res) => {
 app.get('/api', async (req, res) => {
     const query = req.query;
     const YYYYMMDD = 20231010;
-    const data = await axios.get(`https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010144&Type=json&MLSV_YMD=${YYYYMMDD}`);
+    const SchoolCode = 7010144;
+    const data = await axios.get(`https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=${SchoolCode}&Type=json&MLSV_YMD=${YYYYMMDD}`);
     const meal = data.data?.mealServiceDietInfo?.[1]?.row?.[0].DDISH_NM;
     if (!meal) {
         res.status(404).json({ message: 'Not Found' });
