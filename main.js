667401b5a59dd3963a2c312de9b53b1bc33f3a9d
JIN WOOHYUN <helgisnw@gmail.com>
2023-10-10 10:12:53 +0900

    밀 없애고 경로수정

--- main.js
+++ main.js
@@ -13,21 +13,21 @@ app.get("/", async (req, res) => {
 app.get("/api", async (req, res) => {
     const query = req.query;
     const YYYYMMDD = 20231010;
-    const SchoolCode = 7010144;
-    //const SchoolCode = 7010209; //API수정시 등록
+    const SchoolCode = 7010144; //대일고
+    //const SchoolCode = 7010209; //API수정시 등록 양천고
     const data = await axios.get(
         `https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=${SchoolCode}&Type=json&MLSV_YMD=${YYYYMMDD}`
     );
-    const meal = data.data?.mealServiceDietInfo?.[1]?.row?.[0].DDISH_NM;
+    //const meal = data.data?.mealServiceDietInfo?.[1]?.row?.[0].DDISH_NM;
     const carbon = data.data?.mealServiceDietInfo?.[1]?.row?.[0].ORPLC_INFO;
     const calories = data.data?.mealServiceDietInfo?.[1]?.row?.[0].CAL_INFO;
-    if (!meal) {
+    /* if (!carbon) {
         res.status(404).json({ message: "Not Found" });
-    }
-    res.status(200).json({ meal: meal, carbon: carbon, calories: calories });
+    }*/
+    res.status(200).json({ /*meal: meal,*/ carbon: carbon, calories: calories });
 });
 
-app.get("/menu", async (req, res) => {
+app.get("/api/menu", async (req, res) => {
     const query = req.query;
     const headers = {
         "User-Agent":
