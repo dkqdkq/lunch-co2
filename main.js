9677cf1805c9c444c1380545c47e9852e2afbe02
helgisnw <helgisnw@gmail.com>
2023-11-07 22:05:01 +0900

    edit add

--- main.js
+++ main.js
@@ -7,58 +7,49 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-    const menu = await fetch("/api/menu").then((res) => res.json());
-    //res.sendFile(__dirname + "/public/index.html");
-    res.render("/public/index.html", { menu });
+    res.sendFile("/public/index.html");
 });
 
 app.get("/api", async (req, res) => {
     const query = req.query;
-    const YYYYMMDD = 20231010;
-    const SchoolCode = 7010144; //대일고
-    //const SchoolCode = 7010209; //API수정시 등록 양천고
+    const YYYYMMDD = getFormattedDate();
+    const SchoolCode = 7010209;
     const data = await axios.get(
         `https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=${SchoolCode}&Type=json&MLSV_YMD=${YYYYMMDD}`
     );
-    const meal = data.data?.mealServiceDietInfo?.[1]?.row?.[0].DDISH_NM;
-    const carbon = data.data?.mealServiceDietInfo?.[1]?.row?.[0].ORPLC_INFO;
-    const calories = data.data?.mealServiceDietInfo?.[1]?.row?.[0].CAL_INFO;
-    /* if (!carbon) {
+    const meal =
+        data.data?.mealServiceDietInfo?.[1]?.row?.[0]?.DDISH_NM.split("<br/>");
+    const carbon = data.data?.mealServiceDietInfo?.[1]?.row?.[0]?.ORPLC_INFO;
+    const calories = data.data?.mealServiceDietInfo?.[1]?.row?.[0]?.CAL_INFO;
+    if (!meal) {
         res.status(404).json({ message: "Not Found" });
-    }*/
+        return;
+    }
     res.status(200).json({
         success: true,
         data: {
             meal: meal,
-            carbon: carbon,
             calories: calories,
         },
     });
 });
 
-app.get("/api/menu", async (req, res) => {
-    const query = req.query;
-    const headers = {
-        "User-Agent": "Mozilla/5.0",
-    };
-    const YYYYMMDD = 0;
-    const mlsvId = 2376932;
-    const data = await axios.get(
-        `https://yangcheon.sen.hs.kr/dggb/module/mlsv/selectMlsvDetailPopup.do?mlsvId=${mlsvId}`,
-        { headers }
-    );
-    if (!data) {
-        res.status(404).json({ message: "Not Found" });
-    }
-    const $ = cheerio.load(data.data);
-    const menu = $("#detailFrm > table > tbody > tr:nth-child(4) > td").text();
-    /*res.status(200).json({
-        success: true,
-        data: menu.trim().split(","),
-    });*/
-    res.json(menu);
+app.get("/new", async (req, res) => {
+    res.sendFile(__dirname + "/public/new.html");
 });
 
 app.listen(30000, () => {
     console.log("Server is listening on port 30000");
 });
+
+function getFormattedDate() {
+    const today = new Date();
+    console.log(today);
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 1을 더하고 2자리로 만듭니다.
+    const day = String(today.getDate()).padStart(2, "0");
+
+    const formattedDate = `${year}${month}${day}`;
+
+    return formattedDate;
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,41 @@
-const meal = document.querySelector(".meal");
+const mealList = document.querySelector(".meal-list");
+const kcalDiv = document.querySelector(".kcal-data");
+const allergyList = document.querySelector(".allergy-list");
 
-const getMeal = async () => {
-    const res = await fetch("/api/menu")
+const getMealData = async () => {
+    const res = await fetch("/api")
         .then((res) => res.json())
         .then((res) => res.data);
     return res;
 };
 
+const allergyInfo = [
+    "1. 달걀",
+    "2. 우유",
+    "3. 메밀",
+    "4. 땅콩",
+    "5. 대두",
+    "6. 밀",
+    "7. 고등어",
+    "8. 게",
+    "9. 새우",
+    "10. 돼지고기",
+    "11. 복숭아",
+    "12. 토마토",
+    "13. 아황산",
+    "14. 호두",
+    "15. 닭고기",
+    "16. 쇠고기",
+    "17. 오징어",
+    "18. 조개류",
+    "19. 잣",
+];
+
 (async () => {
-    const menu = await getMeal();
-    console.log(menu);
-    const menuList = menu.map((item) => {
+    const { meal, calories } = await getMealData();
+    console.log(meal);
+    console.log(calories);
+    const menuList = meal.map((item) => {
         return `
         <div class="meal-item">
             <div class="meal-name">
@@ -19,5 +44,14 @@ const getMeal = async () => {
         </div>
         `;
     });
-    meal.innerHTML = menuList.join("");
+    mealList.innerHTML = menuList.join("");
+    kcalDiv.innerHTML = `<p>${calories}</p>`;
+    const allergyData = allergyInfo.map((item) => {
+        return `
+        <div class="allergy-item">
+            <p>${item}</p>
+        </div>
+        `;
+    });
+    allergyList.innerHTML = allergyData.join("");
 })();
